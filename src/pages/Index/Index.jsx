cf145bae23392f59fe8a500c9d3e97ef73895b09
krlrmlv <krlrmlv@yandex.ru>
2024-10-22 00:06:09 +0300

    try to fix actions

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Index() {
   return (
     <ul>
       <li>
-      <Link to="/portal">Portal</Link>
+        qdq dq  qddq  
+      {/* <Link to="/portal">Portal</Link> */}
       </li>
       <li>
-      <Link to="/articles">Articles</Link>
+      {/* <Link to="/articles">Articles</Link> */}
       </li>
     </ul>
   );
