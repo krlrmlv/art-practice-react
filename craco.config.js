6061ebd9f7b6351ac58656f78cc5672d00d79a12
krlrmlv <krlrmlv@yandex.ru>
2024-10-21 15:04:25 +0300

    try to fix actions

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 module.exports = {
   webpack: {
+    output: {
+      publicPath: '/art-practice-react/',
+    },
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
